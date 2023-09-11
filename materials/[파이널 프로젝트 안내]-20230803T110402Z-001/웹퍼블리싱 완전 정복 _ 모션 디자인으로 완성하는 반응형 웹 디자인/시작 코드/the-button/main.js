d1a78646416a72447da93f14e322c5cfb524bc27
DoinSeol <tjfehdls@gmail.com>
2023-09-11 23:31:02 +0900

    23.09.11 Final

--- materials/[파이널 프로젝트 안내]-20230803T110402Z-001/웹퍼블리싱 완전 정복 _ 모션 디자인으로 완성하는 반응형 웹 디자인/시작 코드/the-button/main.js
+++ materials/[파이널 프로젝트 안내]-20230803T110402Z-001/웹퍼블리싱 완전 정복 _ 모션 디자인으로 완성하는 반응형 웹 디자인/시작 코드/the-button/main.js
@@ -1,6 +1,9 @@
 const btnEl = document.querySelectorAll('.btn');
 // ...
 btnEl[0].addEventListener('click', () => {
-  alert("Click");
-  btnEl[0].classList.add('loading'); 
+  // alert("Click");
+  btnEl[0].classList.add('loading');
+  setTimeout(() => {
+    btnEl[0].classList.remove('loading')
+  }, 3000)
 })
